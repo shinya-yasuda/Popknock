c7763372169608b266035bfa1736f3fb741cd515
shinya-yasuda <80314330+shinya-yasuda@users.noreply.github.com>
2021-07-14 16:21:39 +0900

    [Add]楽曲追加/編集機能を実装

--- app/controllers/admin/musics_controller.rb
+++ app/controllers/admin/musics_controller.rb
@@ -1,4 +1,5 @@
 class Admin::MusicsController < Admin::BaseController
+  before_action :set_music, only: %i[edit update destroy]
   def index
     @musics = Music.includes(:charts)
   end
@@ -8,18 +9,47 @@ class Admin::MusicsController < Admin::BaseController
   end
 
   def create
-    @music = Music.new(name: params[:music][:name])
-    @music.save!
-    params[:level].each_with_index do |lv, i|
-      chart = Chart.new(music_id: @music.id, difficulty: i, level: lv.to_i)
-      chart.save!
+    genre = music_params[:genre].present? ? music_params[:genre] : music_params[:name]
+    @music = Music.new(name: music_params[:name], genre: genre)
+    if @music.save
+      params[:level].each_with_index do |lv, i|
+        chart = Chart.new(music_id: @music.id, difficulty: i, level: lv)
+        chart.save!
+      end
+      redirect_to admin_musics_path, success: '楽曲を追加しました'
+    else
+      flash.now[:danger] = '楽曲追加に失敗しました'
+      render :new
     end
-    redirect_to levels_path
+  end
+
+  def edit; end
+
+  def update
+    genre = music_params[:genre].present? ? music_params[:genre] : music_params[:name]
+    if @music.update(name: music_params[:name], genre: genre)
+      @music.charts.each_with_index do |chart, i|
+        chart.update(level: params[:level][i])
+      end
+      redirect_to admin_musics_path, success: '楽曲を更新しました'
+    else
+      flash.now[:danger] = '楽曲更新に失敗しました'
+      render :edit
+    end
+  end
+
+  def destroy
+    @music.destroy!
+    redirect_to admin_musics_path
   end
 
   private
 
   def music_params
-    params.require(:music).permit(:name, :level_easy, :level_N, :level_H, :level_EX)
+    params.require(:music).permit(:genre, :name, :level)
+  end
+
+  def set_music
+    @music = Music.includes(:charts).find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
     post 'logout', to: 'user_sessions#destroy', as: :logout
     resources :informations
     resources :musics
+    resources :charts
   end
   get 'login', to: 'user_sessions#new', as: :login
   post 'login', to: 'user_sessions#create'
