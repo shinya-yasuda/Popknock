9fa79bbf829a0cfd9a31a79352be1f98e23fce3c
shinya-yasuda <80314330+shinya-yasuda@users.noreply.github.com>
2021-07-22 20:07:02 +0900

    [Add]乱・S乱難易度表のビューを作成

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,8 +1,16 @@
 class ChartsController < ApplicationController
   def levels; end
 
+  def ran_levels; end
+
+  def s_ran_levels; end
+
   def index
     @level = params[:level]
     @charts = Chart.where(level: @level)
+  end
+
+  def show
+    @chart = Chart.find(params[:id])
   end
 end
