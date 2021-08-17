a709271c59fbda3be54ea1417920074568d78e93
shinya-yasuda <80314330+shinya-yasuda@users.noreply.github.com>
2021-08-17 15:28:49 +0900

    [Add]クリアメダル・ベストスコア・最少BAD表示機能を追加

--- app/models/result.rb
+++ app/models/result.rb
@@ -9,4 +9,34 @@ class Result < ApplicationRecord
   enum medal: { fail_circle: 0, fail_square: 1, fail_star: 2,
                 clear_easy: 3, clear_circle: 4, clear_square: 5, clear_star: 6,
                 fc_circle: 7, fc_square: 8, fc_star: 9, perfect: 10 }
+
+  class << self
+    def best_medal(chart)
+      where(chart_id: chart.id).maximum(:medal)
+    end
+
+    def hi_score(chart)
+      where(chart_id: chart.id).maximum(:score)
+    end
+
+    def least_bad(chart)
+      where(chart_id: chart.id).minimum(:bad)
+    end
+
+    def medal_by_option(option)
+      where(random_option: option).maximum(:medal)
+    end
+
+    def hi_score_by_option(option)
+      where(random_option: option).maximum(:score)
+    end
+
+    def least_bad_by_option(option)
+      where(random_option: option).minimum(:bad)
+    end
+
+    def average_score(option, count)
+      where(random_option: option).order(created_at: :desc).limit(count).average(:score)
+    end
+  end
 end
