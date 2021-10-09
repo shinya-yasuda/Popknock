0a5cffca019f9f865eccc6ed0026c0eabd4049c5
shinya-yasuda <80314330+shinya-yasuda@users.noreply.github.com>
2021-10-09 15:26:46 +0900

    [Fix]csrf認証エラー時にホームへリダイレクト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  rescue_from ActionController::InvalidAuthenticityToken, with: :invalid_token
   add_flash_types :success, :info, :warning, :danger
   before_action :require_login
   before_action :set_current_user
@@ -7,5 +8,9 @@ class ApplicationController < ActionController::Base
 
   def set_current_user
     User.current_user = current_user
+  end
+
+  def invalid_token
+    redirect_to root_path, danger: 'セッションが切れました'
   end
 end
