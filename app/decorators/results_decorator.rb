562822fe089704990d6b97d2d63f669f71d7e28c
shinya-yasuda <80314330+shinya-yasuda@users.noreply.github.com>
2021-10-25 17:25:30 +0900

    [Change]難易度表ページにオプション絞り込み機能を追加

--- app/decorators/results_decorator.rb
+++ app/decorators/results_decorator.rb
@@ -8,12 +8,12 @@ class ResultsDecorator < Draper::CollectionDecorator
   def average_score_level(level)
     total_score = object.pickup_level(level).group('charts.id').maximum(:score).values.sum
     played_count = object.pickup_level(level).group('charts.id').count.size
-    (total_score.to_f / played_count).round(1) if played_count.positive?
+    played_count.positive? ? (total_score.to_f / played_count).round(1) : '-'
   end
 
   def average_bad_level(level)
     total_bad = object.pickup_level(level).group('charts.id').minimum(:bad).values.sum
     played_count = object.pickup_level(level).group('charts.id').count.size
-    (total_bad.to_f / played_count).round(1) if played_count.positive?
+    played_count.positive? ? (total_bad.to_f / played_count).round(1) : '-'
   end
 end
